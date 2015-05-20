db84fc96b12939b7b2c7e8bff75d1dacd72ee371
lmpizarro <lmpbici@gmail.com>
2015-05-20 08:08:34 -0300

    license added

--- gcs/drive_joystick.py
+++ gcs/drive_joystick.py
@@ -1,3 +1,22 @@
+'''
+ * * Copyright (C) 2015 Luis Maria Pizarro <lmpizarro@gmail.com>
+ * *
+ * * This file is part of gauchopiloto.
+ * *
+ * * gauchopiloto is free software; you can redistribute it and/or modify
+ * * it under the terms of the GNU General Public License as published by
+ * * the Free Software Foundation; either version 2, or (at your option)
+ * * any later version.
+ * *
+ * * gauchopiloto is distributed in the hope that it will be useful,
+ * * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * * GNU General Public License for more details.
+ * *
+ * * You should have received a copy of the GNU General Public License
+ * * along with gauchoPiloto; see the file COPYING. If not, see
+ * * <http://www.gnu.org/licenses/>.
+'''
 #!/usr/bin/env python
 #This script requires the following Python modules:
 #  pygame   - http://www.pygame.org/
