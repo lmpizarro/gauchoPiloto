a596fb5e52f4b60b2e6b39014a4a31c90b434583
lmpizarro <lmpbici@gmail.com>
2015-05-20 10:09:55 -0300

    update drive_joystick.py

--- gcs/drive_joystick.py
+++ gcs/drive_joystick.py
@@ -1,23 +1,24 @@
+# -*- coding: utf-8 -*-
+#/usr/bin/python
 '''
- * * Copyright (C) 2015 Luis Maria Pizarro <lmpizarro@gmail.com>
- * *
- * * This file is part of gauchopiloto.
- * *
- * * gauchopiloto is free software; you can redistribute it and/or modify
- * * it under the terms of the GNU General Public License as published by
- * * the Free Software Foundation; either version 2, or (at your option)
- * * any later version.
- * *
- * * gauchopiloto is distributed in the hope that it will be useful,
- * * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * * GNU General Public License for more details.
- * *
- * * You should have received a copy of the GNU General Public License
- * * along with gauchoPiloto; see the file COPYING. If not, see
- * * <http://www.gnu.org/licenses/>.
+* * Copyright (C) 2015 Luis Maria Pizarro <lmpizarro@gmail.com>
+* *
+* * This file is part of gauchopiloto.
+* *
+* * gauchopiloto is free software; you can redistribute it and/or modify
+* * it under the terms of the GNU General Public License as published by
+* * the Free Software Foundation; either version 2, or (at your option)
+* * any later version.
+* *
+* * gauchopiloto is distributed in the hope that it will be useful,
+* * but WITHOUT ANY WARRANTY; without even the implied warranty of
+* * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* * GNU General Public License for more details.
+* *
+* * You should have received a copy of the GNU General Public License
+* * along with gauchoPiloto; see the file COPYING. If not, see
+* * <http://www.gnu.org/licenses/>.
 '''
-#!/usr/bin/env python
 #This script requires the following Python modules:
 #  pygame   - http://www.pygame.org/
 #
